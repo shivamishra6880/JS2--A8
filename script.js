a4e94e79e87f006e176cf7f17e0ae4021a8b2fc8
shivamishra6880 <147376259+shivamishra6880@users.noreply.github.com>
2024-05-25 13:17:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1..------------
+console.log("answer 1 --")
+let i =0;
+let b = 0;
+while(i<1000){
+    if(i % 3 == 0 && i % 5 == 0){
+        b+=i
+       }
+ i++;
+}
+console.log(b)
+
+// 2..------------
+console.log("answer 2 --")
+let arr = [1,12,22,123,49];
+let j = 0;
+let c=0;
+while(j < arr.length){
+   
+    c+= arr[j]
+    j++;
+}
+console.log(c)
+
+// 3.-------
+console.log("answer 3 --")
+let k =7
+while(k <= 31){
+    if(k%2!=0){
+  console.log(k)
+    }
+    k++
+}
+
+// 4.------
+
+// 5.------
+console.log("answer 5 --")
+let arr2 = [13,23,12,45,22,48,66,100]
+let l = 0;
+while(l<arr2.length){
+     if(arr2[l]%2 == 0){
+        console.log(arr2[l])
+     }
+    
+    l++
+}
+
+// 6.------
